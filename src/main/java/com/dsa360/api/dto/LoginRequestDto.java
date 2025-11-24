a816bcf933c00ca8150c48800ccf4c72ae6b085d
the-kiran-academy <thekiranacademyojtdev@gmail.com>
2025-11-24 15:19:27 +0530

    request param to request body

---
+++ src/main/java/com/dsa360/api/dto/LoginRequestDto.java
@@ -0,0 +1,16 @@
+package com.dsa360.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+	
+	private String username;
+	private String password;
+	private String tenantId;
+
+}
