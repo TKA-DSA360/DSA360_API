53686b721d87a07e08580b82db6a1ea2eb2e0204
Ankit Harshe <83650141+25ankit@users.noreply.github.com>
2025-11-24 23:33:44 +0530

    subbranch task1

---
+++ src/main/java/com/dsa360/api/test1/Test.java
@@ -0,0 +1,18 @@
+package com.dsa360.api.test1;
+
+public class Test {
+	
+	 public static void main(String[] args) {
+		 
+		 int n = 5; // size of pattern
+
+	        for (int i = 0; i < n; i++) {
+	            for (int j = 0; j < n; j++) {
+	                System.out.print("* ");
+	            }
+	            System.out.println();
+	        }
+		 
+	 }
+
+}
