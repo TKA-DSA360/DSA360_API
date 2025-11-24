0fce9a9c76638c72d1180f1c45d4ca842373a69a
Ankit Harshe <83650141+25ankit@users.noreply.github.com>
2025-11-24 23:34:14 +0530

    subbranch task 1

--- src/main/java/com/dsa360/api/test1/Test.java
+++ src/main/java/com/dsa360/api/test1/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	 public static void main(String[] args) {
 		 
-		 int n = 5; // size of pattern
+		 int n = 5; 
 
 	        for (int i = 0; i < n; i++) {
 	            for (int j = 0; j < n; j++) {
